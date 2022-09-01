ea0e03ad7b07744ba727b1839ad5ae5d53576e13
phongit <phongit1995@gmail.com>
2022-09-01 21:15:48 +0700

    add id

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -198,6 +198,9 @@ export class ProductService {
     listItem.each(function () {
       const element = Cheerio.load(this);
       const item: any = {};
+      item.id = element('.Product__detail > .Product__bonus').data(
+        'auction-id',
+      );
       item.imageUrl = element('.Product__image > a > img').attr('src');
       item.name = element('.Product__detail > h3 > a').text();
       item.price = element('.Product__detail > .Product__bonus').data(
