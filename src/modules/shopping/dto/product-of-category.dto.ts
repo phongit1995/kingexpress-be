0f0576cf5ac69c278599c2f79d71575b4458574e
Nguyễn Văn Vũ <85126015+vuktmt15@users.noreply.github.com>
2022-09-07 09:04:18 +0700

    slug

--- src/modules/shopping/dto/product-of-category.dto.ts
+++ src/modules/shopping/dto/product-of-category.dto.ts
@@ -1,15 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
 
 export class ProductOfCategoryDto {
-  @ApiProperty({ description: 'name product' })
-  name: string;
+  @ApiProperty({ description: 'name product', required: false })
+  @IsOptional()
+  name?: string;
 
-  @ApiProperty({ description: 'price product' })
-  price: number;
+  @ApiProperty({ description: 'price product', required: false })
+  @IsOptional()
+  price?: number;
 
-  @ApiProperty({ description: 'image product' })
-  image: string;
+  @ApiProperty({ description: 'image product', required: false })
+  @IsOptional()
+  image?: string;
 
-  @ApiProperty({ description: 'url product' })
-  url: string;
+  @ApiProperty({ description: 'url product', required: false })
+  @IsOptional()
+  url?: string;
+
+  @ApiProperty({ description: 'slug shop', required: false })
+  @IsOptional()
+  slugShop?: string;
+
+  @ApiProperty({ description: 'slug product', required: false })
+  @IsOptional()
+  slugProduct?: string;
 }
