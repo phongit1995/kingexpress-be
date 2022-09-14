e8335de5eedd8d4c1aa6c1329a42be627b31f7be
phong nguyen <phongit1995@gmail.com>
2022-09-14 10:19:16 +0700

    add setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { ProductModule } from './modules/product/product.module';
 import { ShoppingModule } from './modules/shopping/shopping.module';
+import { SettingModule } from './modules/setting/setting.module';
 
 @Module({
-  imports: [SharedModule, ProductModule, ShoppingModule],
+  imports: [SharedModule, ProductModule, ShoppingModule, SettingModule],
   controllers: [AppController],
   providers: [AppService],
 })
