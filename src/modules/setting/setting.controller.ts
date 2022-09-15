e1562971eab1c8c283a53bedabe9a882debce059
phong nguyen <phongit1995@gmail.com>
2022-09-15 09:39:08 +0700

    add setting global

--- src/modules/setting/setting.controller.ts
+++ src/modules/setting/setting.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('setting')
-@ApiTags('shopping')
-export class SettingController {}
+@ApiTags('setting')
+export class SettingController {
+  @Get('global')
+  async getSetting() {
+    return {
+      exchangeRate: 220,
+      address:
+        '03-6770-3693 Nhật Bản, 〒136-0075 Tokyo, Koto City, Shinsuna,3 Chome−10−8,  1F Warehouse 3',
+      email: 'kingexpress@gmail.com',
+    };
+  }
+}
