b264a3fdf08f88c52ec1edb35408d5f6ffe4a3ca
phongit <phongit1995@gmail.com>
2022-10-10 22:25:27 +0700

    fix httpRequest

--- src/modules/rakuten/rakuten.module.ts
+++ src/modules/rakuten/rakuten.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RakutenController } from './rakuten.controller';
 import { RakutenService } from './rakuten.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RakutenController],
   providers: [RakutenService],
 })
--- src/modules/rakuten/rakuten.service.ts
+++ src/modules/rakuten/rakuten.service.ts
@@ -1,14 +1,22 @@
+/* eslint-disable max-len */
 import { Injectable } from '@nestjs/common';
-import { RequestService } from 'src/shared/services/request.service';
-
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import Cheerio from 'cheerio';
 @Injectable()
 export class RakutenService {
-  constructor(private requestService: RequestService) {}
+  constructor(private readonly httpService: HttpService) {}
   async getListProductByCategory(categoryId: string, page = 1) {
-    const urlGetCategory = `https://www.rakuten.co.jp/`;
-    console.log(urlGetCategory);
-    const result = await this.requestService.getMethod<string>(urlGetCategory);
-    console.log('result', result);
-    return result;
+    const urlGetCategory = `https://search.rakuten.co.jp/search/mall/-/${categoryId}/?p=${page}`;
+    const { data } = await firstValueFrom(this.httpService.get(urlGetCategory));
+    const $ = Cheerio.load(data);
+    const regExp = /\(([^()]*)\)/g;
+    const totalItemElement = $(
+      '#root > div.dui-container.nav > div.dui-container.infobar > div > div.item.breadcrumb-model.breadcrumb.-fluid > div > span.count._medium',
+    ).text();
+    console.log(totalItemElement);
+    const totalItem = regExp.exec(totalItemElement);
+    console.log(totalItem);
+    return data;
   }
 }
