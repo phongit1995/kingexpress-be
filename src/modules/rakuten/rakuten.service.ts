cf928efb54ec514276d636569c13f1e9bd48af76
phong nguyen <phongit1995@gmail.com>
2022-10-11 14:42:43 +0700

    update get product by category

--- src/modules/rakuten/rakuten.service.ts
+++ src/modules/rakuten/rakuten.service.ts
@@ -8,21 +8,19 @@ export class RakutenService {
   constructor(private readonly httpService: HttpService) {}
   async getListProductByCategory(categoryId: string, page = 1) {
     const urlGetCategory = `https://search.rakuten.co.jp/search/mall/-/${categoryId}/?p=${page}`;
-    console.log(urlGetCategory);
     const { data } = await firstValueFrom<any>(this.httpService.get(urlGetCategory));
     const $ = Cheerio.load(data);
-    const regExp = /\(([^()]*)\)/g;
     const totalItemElement = $(
       '#root > div.dui-container.nav > div.dui-container.infobar > div > div.item.breadcrumb-model.breadcrumb.-fluid > div > span.count._medium',
-    ).text();
-    console.log(totalItemElement);
-    const totalItem = regExp.exec(totalItemElement);
-    console.log(totalItem);
+    )
+      .text()
+      .replace(',', '')
+      .split('（');
+    const totalItem = totalItemElement[1].replace('件）', '');
     const listProductsElement = $(
       '#root > div.dui-container.main > div.dui-container.content > div.dui-container.searchresults > div > div.searchresultitem',
     );
     const listProducts: Array<any> = [];
-    console.log('listProductsElement', listProductsElement.length);
     listProductsElement.each(function () {
       const element = Cheerio.load(this);
       const image = element('div.image > a > img').attr('src');
@@ -30,18 +28,22 @@ export class RakutenService {
       const price = parseInt(
         element('div.content.description.price > span.important').text().replace('円', '').replace(',', ''),
       );
-      console.log(price);
-      console.log('image', image);
-      console.log('url', url);
-      listProducts.push({
-        image,
-        price,
-        url,
-      });
+      const name = element('div.content.title > h2 > a').attr('title');
+      const urlArray = url.replace('https://', '').split('/');
+      if (urlArray[0] == 'item.rakuten.co.jp') {
+        listProducts.push({
+          image,
+          price,
+          url,
+          shipId: urlArray[1],
+          productId: urlArray[2],
+          name,
+        });
+      }
     });
     return {
       results: listProducts,
-      totalProduct: totalItem,
+      totalProduct: parseInt(totalItem),
       pageSize: 50,
     };
   }
